314ea1d5a0848b940e0fe2f881367328af239018
Jpetersen97 <jpeterse@kth.se>
2017-04-05 16:24:50 +0200

    Add files via upload

---
+++ ALARMTEST.ino
@@ -0,0 +1,97 @@
+
+
+
+int bState;
+int buttonPin = 2;
+int aPin[2]= {5,6};
+int count;
+int buzzer = 4;
+
+boolean bFlag;
+boolean aOn;
+boolean aOff;
+boolean alarm;
+boolean stopFlag;
+
+
+
+void setup() {
+
+  delay(3000);
+  Serial.begin(9600);
+  pinMode(buttonPin, INPUT);
+  attachInterrupt(digitalPinToInterrupt(buttonPin), stopAlarm, CHANGE);
+  pinMode(aPin[0], INPUT);
+  pinMode(aPin[1], INPUT);
+  bFlag = true;
+  aOn = true;
+  aOff = true;
+  alarm = false;
+  stopFlag = true;
+
+}
+
+
+
+void loop() {
+
+  if(aOff){
+    aOff = false;
+    Serial.println("Alarm: Off");
+    aOn = true;
+  }
+
+  while(bMode() && stopFlag){
+
+    if(aOn){
+      aOn = false;
+      Serial.println("Alarm: On");
+      aOff = true;
+    }
+
+    alarmState();
+    
+    if(alarm){
+      Serial.println("Alarm!!!");
+      tone(buzzer, 4000,100);
+      noTone;
+      delay(500);
+      tone(buzzer, 4000,100);
+      noTone;
+      delay(500);
+
+
+  stopFlag = true;
+    }
+  }
+}
+
+void stopAlarm(){
+  //Serial.println("Interrupt");
+  alarm = false;
+}
+
+boolean alarmState(){
+ for(int i;i<2;i++){
+  if(digitalRead(aPin[i]) == HIGH){
+    alarm = true;
+  }
+ }
+}
+
+int bMode() {
+  bState = digitalRead(buttonPin);
+  if (bState == HIGH && bFlag == true) {
+    count++;
+    if (count >= 2) {
+      count = 0;
+    }
+    bFlag = false;
+  } 
+  else if (bState == LOW && bFlag == false) {
+    bFlag = true;
+  }
+
+  return count;
+
+}
