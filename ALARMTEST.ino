3bad43299ca0a54624e37039a88fde6ce249917a
Jpetersen97 <jpeterse@kth.se>
2017-04-28 12:18:46 +0200

    Add files via upload

--- ALARMTEST.ino
+++ ALARMTEST.ino
@@ -1,18 +1,27 @@
-
-
+#include <Wire.h>
 
 int bState;
 int buttonPin = 2;
-int aPin[3]= {5,6,7};
+int aPin[3]={6,7,8};
+int pirSensor = 3;
 int count;
+int countL;
 int buzzer = 4;
+int ledPin = 9;
 
+
+int test;
+
+boolean pirOn;
+boolean buttonOff;
+boolean on;
 boolean bFlag;
 boolean aOn;
 boolean aOff;
 boolean alarm;
 boolean stopFlag;
 const byte slaveId = 1;
+byte state = 0;
 
 
 
@@ -25,37 +34,66 @@ void setup() {
   pinMode(aPin[0], INPUT);
   pinMode(aPin[1], INPUT);
   pinMode(aPin[2], INPUT);
-  pinMode(aPin[3], INPUT);
+  pinMode(ledPin, OUTPUT);
+  pinMode(pirSensor, INPUT);
   bFlag = true;
   aOn = true;
   aOff = true;
   alarm = false;
   stopFlag = true;
+  buttonOff = false;
   Wire.onRequest(requestEvent);
   Wire.onReceive(receiveEvent);
-
+  Wire.begin(slaveId);
 }
 
 
 
 void loop() {
-  
-}
+
+
+  if(aOff){
+    aOff = false;
+    Serial.println("Alarm: Off");
+    aOn = true;
+  }
+
+  while(bMode() && stopFlag || on && stopFlag){
+   alarmLoop();
+  }
+ }
+
 
 void stopAlarm(){
   //Serial.println("Interrupt");
   alarm = false;
+  on = false;
+  buttonOff = true;
+  digitalWrite(ledPin, LOW);
 }
 
-boolean alarmState(){
- for(int i;i<3;i++){
-  if(digitalRead(aPin[i]) == LOW){
+
+void hallState(){
+  for(int i=0;i<3;i++){
+  if(digitalRead(aPin[i]) == HIGH){
     alarm = true;
   }
  }
 }
 
+void pirState (){
+  
+  if (digitalRead(pirSensor) == HIGH) {
+      alarm = true;
+  }
+}
+
+
+
+
+
 int bMode() {
+  
   bState = digitalRead(buttonPin);
   if (bState == HIGH && bFlag == true) {
     count++;
@@ -67,46 +105,85 @@ int bMode() {
   else if (bState == LOW && bFlag == false) {
     bFlag = true;
   }
-
-  return count;
-
+  if(buttonOff == true){
+    buttonOff = false;
+    return 0; 
+  }else{
+     return count;
+  }
 }
 
-void requestEvent(){
-  
-  if(aOff){
-    aOff = false;
-    Wire.println("Alarm: Off");
-    aOn = true;
-  }
 
-  while(bMode() && stopFlag){
+void alarmLoop(){
 
+     
     if(aOn){
       aOn = false;
-      Wire.println("Alarm: On");
+      Serial.println("Alarm: On");
       aOff = true;
     }
-
-    alarmState();
+    
+     countL++;
+     if(countL == 200){
+      countL = 0;
+      state = state^1;
+      digitalWrite(ledPin, state);
+      }
+     
+    hallState();
+    if(pirOn == true){
+    pirState();
+    
+    }
     
     if(alarm){
-      Wire.println("Alarm!!!");
+      Serial.println("Alarm!!!");
+      digitalWrite(ledPin, HIGH);
       tone(buzzer, 4000,100);
       noTone;
-      delay(500);
+      delay(200);
       tone(buzzer, 4000,100);
       noTone;
-      delay(500);
-
-
+      delay(200);
+     
   stopFlag = true;
     }
   }
- 
-  }
 
-void receiveEvent(){
-    
+void requestEvent(){
+  if(alarm){
+    Wire.print('1');
+    }else{
+    Wire.print('0');
+    }
   }
+ 
+
+void receiveEvent(int howMany){
+    char inChar;
+    while(Wire.available() > 0){
+      inChar = Wire.read();
+      if (inChar == 'H'){
+        on = true;
+        buttonOff = false;
+        }
+       if(inChar == 'L'){
+        on = false;
+        alarm = false;
+        buttonOff = true;
+        pirOn = false;
+        digitalWrite(ledPin, LOW);
+        }
+        if(inChar == 'X'){
+         pirOn = true;
+        }
+        if(inChar == 'Y'){
+         pirOn = false;
+        }
+      
+   }
+}
+      
+      
+      
 
